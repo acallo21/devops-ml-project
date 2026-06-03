3dc2b982c5f368326b0c147692b3e89471189c89
Aaron Calloway <terpfan911@gmail.com>
2026-06-03 00:13:46 -0400

    Add pip caching to GitHub Actions

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 @app.route("/")
 def home():
-    return "DevOps + MLOps Project is Running!"
+    app.logger.info("Home endpoint accessed")
+    return "App Running"
 
 @app.route("/health")
 def health():
-    return jsonify(status="healthy")
+    return {
+    "status": "healthy",
+    "service": "flask-api",
+    "version": "1.0"
+}, 200
 
 @app.route("/predict")
 def predict():
--- test_app.py
+++ test_app.py
@@ -1,5 +1,36 @@
-import requests
+from app import app
+
 
 def test_home():
-    response = requests.get("http://127.0.0.1:5000")
+    client = app.test_client()
+
+    response = client.get("/")
+
     assert response.status_code == 200
+    assert b"App Running" in response.data
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "healthy"
+    assert data["service"] == "flask-api"
+    assert data["version"] == "1.0"
+
+
+def test_predict():
+    client = app.test_client()
+
+    response = client.get("/predict")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["prediction"] == "coming soon"
